47eb1da12e6ec9795314b59506b80f2d0843c447
Artalk-tn <badboy.dhia@gmail.com>
2021-01-23 02:01:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React , {useEffect,useState, useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinuts, setTimerMinuts] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+  let interval = useRef();
+  const startTimer = ()=> {
+    const countdownDate = new Date('Jan 23, 2021 01:58:00').getTime();
+    interval= setInterval(() => {
+      const now = new Date();
+      const distance= countdownDate- now;
+      const days = Math.floor(  distance / (1000  * 60 * 60 * 24) );
+      const hours = Math.floor(( distance % (1000  * 60 * 60 * 24)) / (1000 * 60 * 60) );
+      const miniuts = Math.floor(( distance % (1000  * 60 * 60)) / (1000 * 60) );
+      const seconds = Math.floor(( distance % (1000  * 60 ))/ 1000 );
+      if (distance < 0 ){
+        clearInterval(interval.current);
+      }else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinuts(miniuts);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(()=> {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    }
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section className="timer-container">
+        <section className="timer">
+          <div>
+            <span className="mdi mdi-calendar-clock timer-icon"></span>
+            <h2>Coundown Timer</h2>
+            <p>lorem ipsum abcde hahaah hello ifeifji zzz hello</p>
+          </div>
+          <div>
+            <section>
+              <p>{timerDays}</p>
+              <p><small>Days</small></p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerHours}</p>
+              <p><small>Hours</small></p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerMinuts}</p>
+              <p><small>Minuts</small></p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerSeconds}</p>
+              <p><small>Seconds</small></p>
+            </section>
+          </div>
+        </section>
+      </section> 
   );
 }
 
